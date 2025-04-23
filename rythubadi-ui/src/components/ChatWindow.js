a18dfe7a10e935103dc586854f5fd085b354b60f
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-04-22 20:25:58 -0400

    moved logout to chat window component to align properly

--- rythubadi-ui/src/components/ChatWindow.js
+++ rythubadi-ui/src/components/ChatWindow.js
@@ -1,14 +1,28 @@
 import ChatInput from "./ChatInput"
+import { useNavigate } from "react-router-dom";
 import './ChatWindow.css'
 
-function ChatWindow() {
+function ChatWindow({email}) {
 
     const messages = [
         {id: 1, sender: "user", text: "Hello"},
         {id: 2, sender: "bot", text: "Hello, how can i help you?"},
     ]
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('email');
+        navigate("/")
+    }
+
     return (
         <div className="chat-window">
+        <div className="top-right-section">
+          <h3>Welcome {email}</h3>
+          <button onClick={handleLogout}>logout</button>
+        </div>
             <div className="chat-messages">
                 {messages.map((message) => {
                    return <div className="chat-message"> 
