50a11e3de7ee4abe9a202020d8ae2cf0a0f1ed3d
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-08-10 20:45:38 -0400

    new line

--- rythubadi-AI-utils/rb_app.py
+++ rythubadi-AI-utils/rb_app.py
@@ -7,3 +7,4 @@ os.environ['LANGLANGCHAIN_PROJECTCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY"
 os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')
 os.environ['LANGCHAIN_TRACING_V2'] = os.getenv('LANGCHAIN_TRACING_V2')
 
+
