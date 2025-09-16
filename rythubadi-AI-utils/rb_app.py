7333e14bd98fccb5f0bfd91eb659e7c8c903a999
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-09-15 20:20:40 -0400

    rb init

--- rythubadi-AI-utils/rb_app.py
+++ rythubadi-AI-utils/rb_app.py
@@ -1,3 +1,5 @@
-# load docs and store in vector db 
+import os
 
-# and reload them from db and query it
+os.environ['LANGLANGCHAIN_PROJECTCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
+os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')
+os.environ['LANGCHAIN_TRACING_V2'] = os.getenv('LANGCHAIN_TRACING_V2')
