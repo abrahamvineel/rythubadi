828fd8c83cf27ce50d82301de277c60cc3bacd65
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-11-24 09:41:20 -0500

    Update rb_app.py

--- rythubadi-AI-utils/rb_app.py
+++ rythubadi-AI-utils/rb_app.py
@@ -4,6 +4,7 @@ from langchain.embeddings import OpenAIEmbeddings
 embeddings = OpenAIEmbeddings()
 vectordb = FAISS.load_local("pdf_index", embeddings)
 
+
 query = "how to check soil moisture?"
 docs = vectordb.similarity_search(query, k=3)
 print(docs[0].page_content)
