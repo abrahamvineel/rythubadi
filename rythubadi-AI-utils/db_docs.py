85a1ce404c428874d3bfdf6298cef9c913f30f20
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2026-01-07 20:06:16 -0500

    Update db_docs.py

--- rythubadi-AI-utils/db_docs.py
+++ rythubadi-AI-utils/db_docs.py
@@ -12,7 +12,6 @@ folder_path = "./sample_pdfs"
 
 all_docs = []
 
-
 for file in os.listdir(folder_path):
     if file.endswith(".pdf"):
         loader = PyPDFLoader(os.path.join(folder_path, file))
